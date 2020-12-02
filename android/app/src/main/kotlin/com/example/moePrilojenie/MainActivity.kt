70f35c7b279855fa2fce8d2ba1a4401ebaa3c93a
AmirHai <59731951+AmirHai@users.noreply.github.com>
2020-12-02 18:38:39 +0300

    инициализация проекта

---
+++ android/app/src/main/kotlin/com/example/moePrilojenie/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.moePrilojenie
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/chatting_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class MyChattingPage extends StatefulWidget{
+  createState() => new MyChattingPageState();
+}
+
+class MyChattingPageState extends State<MyChattingPage>{
+  @override
+  Widget build(BuildContext context){
+
+  }
+}
