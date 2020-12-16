a12f6a923f3440c717fef3ffc0cc5db351e75c47
AmirHai <59731951+AmirHai@users.noreply.github.com>
2020-12-16 18:15:14 +0300

    сделано новое окно и пока что просто показан файл регистрации

---
+++ lib/auntification/login_parol_page.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:moePrilojenie/pages/chatting_page.dart';
+
+class LoginAndParol extends StatefulWidget {
+  createState() => new LogParState();
+}
+
+class MyFormLogPar extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => MyFormLogParState();
+}
+
+class MyFormLogParState extends State{
+  final _formkey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(padding: EdgeInsets.all(10.0),
+        child: new Form(key: _formkey, child: new Column(
+          children: <Widget>[
+            new SizedBox(height: 100,),
+            new TextFormField(validator: (value) {
+              if(value.isEmpty){
+                return 'пожалуйста введите Email';
+              }else{
+                return value;
+              }},
+              decoration: InputDecoration(hintText: 'Email'),
+            ),
+
+            new SizedBox(height: 30,),
+
+            new TextFormField(validator: (value){
+              if(value.isEmpty){
+                return 'пожалуйста введите пароль';
+              }else{
+                return value;
+              }
+            },
+              decoration: InputDecoration(hintText: 'Password'),
+            ),
+
+            new SizedBox(height: 75,),
+
+            new Row(
+              children: [
+                new RaisedButton(onPressed: (){
+                  if(_formkey.currentState.validate()) Scaffold.of(context).showSnackBar(SnackBar(content:
+                  Text('вы успешно вошли в аккаунт'), backgroundColor: Colors.green,));
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => MyChattingPage()));
+                }, child: Text('Войти'), color: Colors.deepPurple, textColor: Colors.white,
+                )
+              ],
+            )
+          ],
+        ),
+        )
+    );
+  }
+}
+
+class LogParState extends State<LoginAndParol> {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('аутентификация'),
+          backgroundColor: Colors.red,
+          toolbarHeight: 60,
+        ),
+        body: MyFormLogPar(),
+      ),
+    );
+  }
+}
--- lib/pages/chatting_page.dart
+++ lib/pages/chatting_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class MyChattingPage extends StatefulWidget{
   createState() => new MyChattingPageState();
@@ -7,6 +8,17 @@ class MyChattingPage extends StatefulWidget{
 class MyChattingPageState extends State<MyChattingPage>{
   @override
   Widget build(BuildContext context){
-
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: new Scaffold(
+          appBar: new AppBar(title: new Text('моя прилка')),
+          body: new Center(
+            child: new Column(
+              children: [new Text('первый запуск'), new FlatButton(onPressed: null,
+                  child: Text('типа кнопка'))],
+            )
+          ),
+        )
+      );
   }
 }
